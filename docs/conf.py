2ee8329a92c9039285a7e46c7ceb4b40c22d6c1d
Kazuya Takei <myself@attakei.net>
2020-03-29 15:33:52 +0900

    Documentation configuration

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+# -- Path setup --------------------------------------------------------------
+
+
+# -- Project information -----------------------------------------------------
+project = 'fastapi-for-firebase'
+copyright = '2020, Kazuya Takei'
+author = 'Kazuya Takei'
+release = '0.0.2'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+fastapi-for-firebase
+====================
+
+FastAPI support functions for Firebase backend
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
